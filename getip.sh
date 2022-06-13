8d655cf4ea99aeda4765eb8ac7ffb3ce6aabbb0e
merkleID <luca@merkle.id>
2022-06-13 17:40:29 +0200

    first commit
    script block tor from iptables

---
+++ getip.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+curl -sSL "https://check.torproject.org/cgi-bin/TorBulkExitList.py?ip=$(curl icanhazip.com)" | sed '/^#/d' > /tmp/list1.txt
+curl 'https://check.torproject.org/torbulkexitlist?ip=' > /tmp/list2.txt
+curl -sSL "https://www.dan.me.uk/torlist/?ip=$(curl icanhazip.com)" | sed '/^#/d' > /tmp/dan.txt
+
+cat /tmp/list1.txt /tmp/list2.txt /tmp/dan.txt | sort | uniq > /tmp/final.txt
+
+ipset flush tor
+iptables -D INPUT -m set --match-set tor src -j DROP
+while read IP; do ipset -q -A tor $IP; done
+iptables -A INPUT -m set --match-set tor src -j DROP
+
+rm -rf /tmp/lista?.txt /tmp/dan.txt /tmp/final.txt
